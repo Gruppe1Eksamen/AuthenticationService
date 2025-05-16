16f3434065979f2a107c983d43b26bdf0d036ee1
NicklasEAA <eaa23nrfa@students.eaaa.dk>
2025-05-16 09:54:34 +0200

    nginx.conf og vault init script

--- obj/Debug/net8.0/AuthService.AssemblyInfo.cs
+++ obj/Debug/net8.0/AuthService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd7900576a470f4ba57257b2b33f8b24f415adf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d39ec0d579503afce1d71e4028c634d307a7676")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
